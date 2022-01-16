83d803d3392c5a1cfc522dfc659d954a250fa013
bmihretie <birhanu13@gmail.com>
2022-01-16 13:36:45 -0600

    similar pages created,confirm password, city, state,Zipcode,popup dialog for terms and condtions, paginated table for credit history and transactions....

--- src/app/credit-score/credit-score.component.ts
+++ src/app/credit-score/credit-score.component.ts
@@ -1,23 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { HttpclientService } from '../service/httpclient.service';
 @Component({
   selector: 'app-credit-score',
   templateUrl: './credit-score.component.html',
   styleUrls: ['./credit-score.component.css']
 })
-export class CreditScoreComponent implements OnInit {
+export class CreditScoreComponent implements OnInit,AfterViewInit {
 
   constructor(private httpClientService:HttpclientService) { }
 
   creditScores: any;
+ 
+
+  // fields for toggling hide and show button for credit card history
+  isCreditHistoryShown:boolean = false;
+  buttonName:string='Get Credit Score History';
+
+
+  // fields for pagination table column
+  displayedColumns: string[] = ['creditScore', 'statementDate','description'];
+  
+ // a field that holds the data for credit history information
+  dataSource !: MatTableDataSource<any>;
+
+  @ViewChild('paginator')
+  paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit() {
     this.httpClientService.getCustomerStatementsAndCreditScore().subscribe(
-      response =>{
+      (response:any) =>{ 
         console.log(response)
-        this.creditScores = response
+        this.creditScores = response;
+        this.dataSource = new MatTableDataSource(response);
+        this.dataSource.paginator = this.paginator;
       },
     )
+  
   }
+
+  
+
+
+  toggleCreditHistoryShow() {
+
+  this.isCreditHistoryShown = ! this.isCreditHistoryShown;
+  if(this.isCreditHistoryShown==true){
+    this.buttonName ='Hide Credit Score History';
+    
+    
+  }
+  else{
+    this.buttonName='Get Credit Score History';
+  }
+  
+
+  }
+
+  
 
 }
